74c5fd64e5811bdd58eaeff031061f44402a195b
kurita373 <minami0220252525@gmail.com>
2020-09-11 20:13:20 +0900

    headerとfooterphp作ったよ

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <title>不思議な魅力cuba</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+ <link href="https://fonts.googleapis.com/css2?family=Marck+Script&display=swap" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+ 
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/skeleton.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+  
+  <!-- js-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ 
+  <meta name="google-site-verification" content="lhJmuvzQwWwrpzxY9V40CiYSfNtPP9Yme8hUn4gatOw" />
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <?php wp_head(); ?>
+    </head>
+  
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        
+       <div class="topimg">
+           <img class="sp" src="<?php echo get_template_directory_uri(); ?>/images/topsp.jpg" alt="top画像">
+           <img class="pc" src="<?php echo get_template_directory_uri(); ?>/images/toppc.jpg" alt="top画像">
+           <div class="box1"></div>
+           
+           <p class="comment fadein">不思議な魅力にきっとあなたも虜になる</p>
+           <h1 class="fadein">CUBA</h1>
+       </div>
+    </header>
--- index.php
+++ index.php
@@ -1,50 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <title>不思議な魅力cuba</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
- <link href="https://fonts.googleapis.com/css2?family=Marck+Script&display=swap" rel="stylesheet">
- <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet">
- 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/skeleton.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-  
-  <!-- js-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- 
-  <meta name="google-site-verification" content="lhJmuvzQwWwrpzxY9V40CiYSfNtPP9Yme8hUn4gatOw" />
-  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  <?php wp_head(); ?>
-    </head>
-  
-</head>
-<body <?php body_class(); ?>>
-    <header>
-        
-       <div class="topimg">
-           <img class="sp" src="<?php echo get_template_directory_uri(); ?>/images/topsp.jpg" alt="top画像">
-           <img class="pc" src="<?php echo get_template_directory_uri(); ?>/images/toppc.jpg" alt="top画像">
-           <div class="box1"></div>
-           
-           <p class="comment fadein">不思議な魅力にきっとあなたも虜になる</p>
-           <h1 class="fadein">CUBA</h1>
-       </div>
-    </header>
+<?php get_header(); ?>
     <main>
     
     <div class="loopsample">
@@ -152,24 +106,5 @@
     
         
     </main>
-    <footer>
-        <p class="f1">©２０２０︎minami</p>
-    </footer>
-    <script>
-    $(function(){
-    $(window).scroll(function (){
-        $('.fadein').each(function(){
-            var targetElement = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > targetElement - windowHeight + 200){
-                $(this).css('opacity','1');
-                $(this).css('transform','translateY(0)');
-            }
-        });
-    });
-});
-</script>
-<?php wp_footer(); ?>
-</body>
-</html>
+    
+    <?php get_footer(); ?>
